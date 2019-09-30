d79bab4eed8389d937dbf13d2dc89440e43f9fb7
Omar <o.mohammed@morniksa.com>
2019-09-30 12:00:54 +0200

    add config

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ if (process.env['NODE_ENV'] == 'production') {
     vehicles_host = 'http://vehicles-app.us-west-2.elasticbeanstalk.com';
     customers_host = 'http://alten-customers-dev.us-west-2.elasticbeanstalk.com';
     // realtime_host = 'http://alten-realtime-dev.us-west-2.elasticbeanstalk.com:80'
-    realtime_host = 'https://alten-realtime.herokuapp.com'
+    realtime_host = 'http://alten-realtime.herokuapp.com'
     // vehicles_host = 'http://localhost:8000';
     // customers_host = 'http://localhost:8001';
     // realtime_host = 'http://localhost:3001';
