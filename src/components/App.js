aa388ee90d542850ccc497f202b83916980eb170
Omar <o.mohammed@morniksa.com>
2019-09-30 10:58:56 +0200

    test

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
 
 function App() {
+  console.log(process.env)
   return (
     <div className="container-fluid">
       <ToastContainer autoClose={3000} hideProgressBar />
