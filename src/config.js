d4a4f52e0e3b1f00b0c89f279ea258b5a3679ac9
Omar <o.mohammed@morniksa.com>
2019-09-30 09:16:01 +0200

    add https

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ let vehicles_host;
 let customers_host;
 let realtime_host;
 if (process.env['NODE_ENV'] == 'production') {
-    vehicles_host = 'http://vehicles-app.us-west-2.elasticbeanstalk.com';
-    customers_host = 'http://alten-customers-dev.us-west-2.elasticbeanstalk.com';
+    vehicles_host = 'https://vehicles-app.us-west-2.elasticbeanstalk.com';
+    customers_host = 'https://alten-customers-dev.us-west-2.elasticbeanstalk.com';
     realtime_host = ''
 } else {
     vehicles_host = 'http://localhost:8000';
