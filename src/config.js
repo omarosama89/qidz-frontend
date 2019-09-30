749a200dc5cf1406b87487adb0d6038253e01ee4
Omar <o.mohammed@morniksa.com>
2019-09-30 18:55:43 +0200

    realtimehosn=tname

--- src/config.js
+++ src/config.js
@@ -4,14 +4,14 @@ let realtime_host;
 if (process.env['NODE_ENV'] == 'production') {
     vehicles_host = 'http://vehicles-app.us-west-2.elasticbeanstalk.com';
     customers_host = 'http://alten-customers-dev.us-west-2.elasticbeanstalk.com';
-    realtime_host = 'http://alten-realtime-dev.us-west-2.elasticbeanstalk.com'
+    realtime_host = 'http://alten-realtime.herokuapp.com'
 } else {
     vehicles_host = 'http://vehicles-app.us-west-2.elasticbeanstalk.com';
     customers_host = 'http://alten-customers-dev.us-west-2.elasticbeanstalk.com';
     // realtime_host = 'http://alten-realtime-dev.us-west-2.elasticbeanstalk.com:80'
-    realtime_host = 'http://alten-realtime.herokuapp.com'
     // vehicles_host = 'http://localhost:8000';
     // customers_host = 'http://localhost:8001';
+    realtime_host = 'http://alten-realtime.herokuapp.com'
     // realtime_host = 'http://localhost:3001';
 }
 export default {
